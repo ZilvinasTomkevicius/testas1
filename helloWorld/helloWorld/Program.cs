9aab54307e661208776a6420fe91f073931c4c1c
ZilvinasTomkevicius <zilvinosasas@gmail.com>
2016-08-17 17:14:49 +0300

    Revert "Revert "testas1""
    This reverts commit 6befc68ac5e6ac73f351e9a4fa5acb069fd8c97b.

---
+++ helloWorld/helloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class conversion1{
+    public static int Main()
+{
+    int i, j, h;
+    bool boolean = true;
+    i = Convert.ToInt32(boolean);
+   Console.WriteLine(i);
+   bool boolean1 = false;
+   j = Convert.ToInt32(boolean1);
+    Console.WriteLine(j);
+    Console.ReadKey();
+        
+    
+        return 0 ;
+    }
+}
