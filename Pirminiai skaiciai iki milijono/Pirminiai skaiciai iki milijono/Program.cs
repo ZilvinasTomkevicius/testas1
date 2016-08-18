c959c5120074201cc14fc5c7ffee036787cfb1f8
ZilvinasTomkevicius <zilvinosasas@gmail.com>
2016-08-18 10:25:54 +0300

    naujos programos

---
+++ Pirminiai skaiciai iki milijono/Pirminiai skaiciai iki milijono/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirminiai_skaiciai_iki_milijono
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+            int x = 100;
+            int k = 0;
+            for(int i = 1; i<=x ;i++)
+            {
+                if (i == 1) Console.WriteLine(i);
+                for (  j = 2; j <= i;j++ )
+                {
+                    
+                    if(i==j) Console.WriteLine(i);
+                    if (i % j == 0) break;
+                   
+                }
+                  
+            }
+             
+            Console.ReadKey();
+        }
+    }
+}
---
+++ bandymaiSuKnyga/bandymaiSuKnyga/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class Bandymas
+{
+    public static int Main(string[] args)
+    {
+        Console.WriteLine("iveskite produktus per kableli");
+        string produktai = Console.ReadLine();
+
+        int produktuSkaicius = 0;
+
+        foreach(char kablelis in produktai)
+        if (kablelis == ',') produktuSkaicius++;
+
+        produktuSkaicius = produktuSkaicius +1;
+
+        Console.WriteLine("ivesti produktai: " + produktai + " prodouktu skaicius: " + produktuSkaicius);
+        Console.ReadKey();
+       
+
+
+        return 0;
+
+    }
+}
+   
+
+
+
