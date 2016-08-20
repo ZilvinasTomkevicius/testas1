f462b161ad09fa055dad2f89d73a3b2e0c10e7ea
ZilvinasTomkevicius <zilvinosasas@gmail.com>
2016-08-20 11:51:39 +0300

    konvertavimas is skaiciu i raides ir raidziu kiekis zodyje

---
+++ konvertavimas Is Skaiciu I raides/konvertavimasIsSkaiciuIraides/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konvertavimasIsSkaiciuIraides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 31; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 44) Console.WriteLine(i + " d");
+                if (i == 65) Console.WriteLine(i + " u");
+                if (i == 59) Console.WriteLine(i + " o");
+                if (i == 58) Console.WriteLine(i + " n");
+                if (i == 41) Console.WriteLine(i + " a");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ raidziu skaicius zodyje/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace raidziuSkaiciusZodyje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // si programa skaiciuoja, kiek yra ivestu raidziu ivestame zodyje
+            int raidziuSkaicius = 0;
+            Console.WriteLine("iveskite Zodi");
+            string zodis = Console.ReadLine();
+            Console.WriteLine("iveskite raide");
+            string raideKuriaIvedame = Console.ReadLine();                                           
+            foreach(char raideKuriosMumsReikia in zodis)
+            {
+                //konvertuojame is string duom. tipo i char, kad veliau galetume sulyginti
+                char raideTampaRaide = Convert.ToChar(raideKuriaIvedame);
+                if (raideTampaRaide == raideKuriosMumsReikia) raidziuSkaicius++;
+            }
+            Console.WriteLine("raidziu skaicius = {0}", raidziuSkaicius);
+            Console.ReadKey();
+           
+
+        }
+    }
+}
