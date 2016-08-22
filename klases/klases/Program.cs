389c04854b8b25b0cb43983e0ebfedabbe206da7
ZilvinasTomkevicius <zilvinosasas@gmail.com>
2016-08-22 13:55:08 +0300

    klases

---
+++ klases/klases/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klases
+{
+    class Program
+    {
+        class Produktai
+        {
+            class VaisiaiDarzoves
+            {
+                string[] Vaisiai;
+                string[] Darzoves;
+                string[] Uogos;
+                string[] Grybai;
+            }
+            class Mesa
+            {
+                string[] rusis; //jautiena, kiauliena, paukstiena..
+                string[] busena; //saldyta, rukyta..
+                string[] gaminiai; //desreles, desra, kumpis..
+                string[] dalis; //kulsis, nugarine, sprandine..
+            }
+            class Grudai
+            {
+                string[] miltai;
+                string[] duona;
+                string[] makaronai;
+            }
+            string[] pienoProduktai;
+            string[] pagalbiniaiProduktai; // mieles, druska..
+            string[] saldumynai;
+            }
+        struct receptas
+        {
+            string[] reikalingiProduktai;
+            string procesas;
+            int paruosimoLaikas;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
